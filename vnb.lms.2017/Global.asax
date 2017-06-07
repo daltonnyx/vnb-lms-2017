3f23dba826211fab918b2ddfcb761cdda839961c
aquez <aquez@AQUEZ-PC>
2017-06-08 00:31:34 +0700

    Init Project

---
+++ vnb.lms.2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vnb.lms._2017.MvcApplication" Language="C#" %>
---
+++ vnb.lms.2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vnb.lms._2017.Startup))]
+namespace vnb.lms._2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
